a95f88bcec5c4277a9cac81c9241a90e7b5618ad
Matias Isella <matias.isella@gmail.com>
2012-02-29 12:27:24 -0300

    First commit

---
+++ js/jq-menu.js
@@ -0,0 +1,59 @@
+(function($){
+
+    $.fn.menu = function(options){
+	
+			var settings = $.extend({
+				"header_selector"	:	"H2",
+				"force_one"			:	true,
+				"speed"				:	100
+			}, options);
+						
+            return this.each(function(){
+			
+				$(this).data("options", settings);
+				
+			    var $this = $(this),
+				$headers = $($this.data("options").header_selector,$this);		
+
+				$this.addClass("jq-menu");
+				$headers.addClass("jq-menu-header");
+				
+				$headers.each(function(){
+										
+					var $child = $(this).next("ul");
+					
+					if ($child.length)
+						$("a",this).append("<em>+<em>");
+					else
+						return;
+												
+					$(this).click(function(){
+					
+						var $em = $("em",this);
+						
+						if ($this.data("options").force_one) $("ul").not($child).slideUp($this.data("options").speed);
+						
+						$child.slideToggle($this.data("options").speed,function(){checkSymbol()});
+						
+					})
+					
+					function checkSymbol(){
+						$headers.each(function(){
+						
+							var $child = $(this).next("ul"),
+							$em = $("em",this);
+							
+							if (!$child.length) return;
+							
+							($child.is(":visible")) ? $em.text("-") : $em.text("+");							
+							
+						})
+					}
+					
+				})
+
+            });
+		
+    };
+	
+})(jQuery);
